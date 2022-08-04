8231c6776b3e2192060efd41ed06c85778b96895
Dennis <adigwedennis@yahoo.com>
2022-08-04 16:39:42 +0100

    sign up layout done

--- app/src/main/java/com/denniscode/smack2/LoginActivity.kt
+++ app/src/main/java/com/denniscode/smack2/LoginActivity.kt
@@ -1,11 +1,17 @@
 package com.denniscode.smack2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+    fun signUpBtnClicked (view: View) {
+        val signUpIntent = Intent (this, SignupActivity::class.java)
+        startActivity(signUpIntent)
     }
 }
